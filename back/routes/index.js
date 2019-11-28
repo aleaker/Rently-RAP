cd3ebfd58ac6a3d116f7db5a2edc404e89291fc9
ssd4045 <n.s_61059032_cdi@hotmail.com>
2019-11-28 11:49:40 -0300

    ahora hay catch para todas las rutas

--- back/routes/index.js
+++ back/routes/index.js
@@ -18,34 +18,54 @@ router.use("/registerRental", registerRentalRouter);
 
 //VER LOS USUARIOS: GET http://localhost:3000/api/users
 router.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //VER UN USER POR SU ID: GET http://localhost:3000/api/user/:id
 router.get("/user/:id", async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.json(user);
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //CREAR UN USER : POST http://localhost:3000/api/user
 router.post("/user", (req, res) => {
-  User.create(req.body);
-  console.log("User creado");
-  res.json({ status: "user created" });
+  try {
+    User.create(req.body);
+    console.log("User creado");
+    res.json({ status: "user created" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //ACTUALIZAR DATA DE UN USER: PUT http://localhost:3000/api/user/:id
 router.put("/user/:id", async (req, res) => {
-  const user = req.body;
-  await User.findByIdAndUpdate(req.params.id, user);
-  res.json({ status: "user updated" });
+  try {
+    const user = req.body;
+    await User.findByIdAndUpdate(req.params.id, user);
+    res.json({ status: "user updated" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //Borrar un usuario /* ARREGLAR
 router.delete("user/:id", async (req, res) => {
-  await User.findByIdAndRemove(req.params.id);
-  res.json({ status: "user deleted" });
+  try {
+    await User.findByIdAndRemove(req.params.id);
+    res.json({ status: "user deleted" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //------------------------------------------------
@@ -54,28 +74,44 @@ router.delete("user/:id", async (req, res) => {
 
 //VER TODAS LAS EMPRESAS: GET http://localhost:3000/api/company
 router.get("/company", async (req, res) => {
-  const company = await Company.find();
-  res.json(company);
+  try {
+    const company = await Company.find();
+    res.json(company);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //VER UNA EMPRESA POR SU ID: GET http://localhost:3000/api/company/:id
 router.get("/company/:id", async (req, res) => {
-  const company = await Company.findById(req.params.id);
-  res.json(company);
+  try {
+    const company = await Company.findById(req.params.id);
+    res.json(company);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //CREAR UNA EMPRESA: POST http://localhost:3000/api/company
 router.post("/company", (req, res) => {
-  Company.create(req.body);
-  console.log("Compania guardada");
-  res.json({ status: "Company created" });
+  try {
+    Company.create(req.body);
+    console.log("Compania guardada");
+    res.json({ status: "Company created" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //ACTUALIZAR DATA DE UNA EMPRESA: PUT http://localhost:3000/api/company/:id
 router.put("/company/:id", async (req, res) => {
-  const company = req.body;
-  await Company.findByIdAndUpdate(req.params.id, company);
-  res.json({ status: "Company updated" });
+  try {
+    const company = req.body;
+    await Company.findByIdAndUpdate(req.params.id, company);
+    res.json({ status: "Company updated" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //----------------------------------------------
@@ -84,29 +120,45 @@ router.put("/company/:id", async (req, res) => {
 
 //VER TODAS LAS RENTADORAS: GET http://localhost:3000/api/carRental
 router.get("/carRental", async (req, res) => {
-  const carRental = await CarRental.find();
-  res.json(carRental);
-  res.redirect("/api/carRental");
+  try {
+    const carRental = await CarRental.find();
+    res.json(carRental);
+    res.redirect("/api/carRental");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //VER UNA RENTADORA POR SU ID: GET http://localhost:3000/api/carRental/:id ARREGLAR *
 router.get("/carRental/:id", async (req, res) => {
-  const carRental = await CarRental.findById(req.params.id);
-  res.json(carRental);
+  try {
+    const carRental = await CarRental.findById(req.params.id);
+    res.json(carRental);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //CREAR UNA RENTADORA: POST http://localhost:3000/api/carRental
 router.post("/carRental", (req, res) => {
-  CarRental.create(req.body);
-  console.log("Rentadora guardada");
-  res.redirect("/api/carRental");
+  try {
+    CarRental.create(req.body);
+    console.log("Rentadora guardada");
+    res.redirect("/api/carRental");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //ACTUALIZAR DATA DE UNA RENTADORA: PUT http://localhost:3000/api/carRental/:id
 router.put("/carRental/:id", async (req, res) => {
-  const carRental = req.body;
-  await CarRental.findByIdAndUpdate(req.params.id, carRental);
-  res.json({ status: "CarRental updated" });
+  try {
+    const carRental = req.body;
+    await CarRental.findByIdAndUpdate(req.params.id, carRental);
+    res.json({ status: "CarRental updated" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //----------------------------------------
@@ -115,28 +167,44 @@ router.put("/carRental/:id", async (req, res) => {
 
 //VER TODAS LAS COMISIONES: GET http://localhost:3000/api/commission
 router.get("/commission", async (req, res) => {
-  const commission = await Commission.find();
-  res.json(commission);
+  try {
+    const commission = await Commission.find();
+    res.json(commission);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //VER UNA COMISION POR SU ID: GET http://localhost:3000/api/commission/:id
 router.get("/commission/:id", async (req, res) => {
-  const user = await Commission.findById(req.params.id);
-  res.json(user);
+  try {
+    const user = await Commission.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //CREAR UNA COMISION: POST http://localhost:3000/api/commission
 router.post("/commission", (req, res) => {
-  Commission.create(req.body);
-  console.log("Comision guardada");
-  res.redirect("/api/commission");
+  try {
+    Commission.create(req.body);
+    console.log("Comision guardada");
+    res.redirect("/api/commission");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //ACTUALIZAR DATA DE UNA COMISION: PUT http://localhost:3000/api/commission/:id
 router.put("/commission/:id", async (req, res) => {
-  const commission = req.body;
-  await Commission.findByIdAndUpdate(req.params.id, commission);
-  res.json({ status: "Commission updated" });
+  try {
+    const commission = req.body;
+    await Commission.findByIdAndUpdate(req.params.id, commission);
+    res.json({ status: "Commission updated" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //------------------------------------------
@@ -145,28 +213,44 @@ router.put("/commission/:id", async (req, res) => {
 
 //VER TODAS LAS RESERVAS: GET http://localhost:3000/api/booking
 router.get("/booking", async (req, res) => {
-  const booking = await Booking.find();
-  res.json(booking);
+  try {
+    const booking = await Booking.find();
+    res.json(booking);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //VER UNA RESERVA POR SU ID: GET http://localhost:3000/api/booking/:id
 router.get("/booking/:id", async (req, res) => {
-  const booking = await Booking.findById(req.params.id);
-  res.json(booking);
+  try {
+    const booking = await Booking.findById(req.params.id);
+    res.json(booking);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //CREAR UNA RESERVA: POST http://localhost:3000/api/booking
 router.post("/booking", (req, res) => {
-  Booking.create(req.body);
-  console.log("Reserva guardada");
-  res.redirect("/api/booking");
+  try {
+    Booking.create(req.body);
+    console.log("Reserva guardada");
+    res.redirect("/api/booking");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //ACTUALIZAR DATA DE UNA RESERVA: PUT http://localhost:3000/api/booking/:id
 router.put("/booking/:id", async (req, res) => {
-  const booking = req.body;
-  await Booking.findByIdAndUpdate(req.params.id, booking);
-  res.json({ status: "Booking updated" });
+  try {
+    const booking = req.body;
+    await Booking.findByIdAndUpdate(req.params.id, booking);
+    res.json({ status: "Booking updated" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
